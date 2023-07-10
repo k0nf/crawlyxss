2198d4997c158cb99c495763f4eb7494bf34783c
Osman Elezi <89322194+k0nf@users.noreply.github.com>
2023-07-10 09:36:23 +0200

    read params from input url on brute

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from urllib.parse import urlparse, urlencode
+from urllib.parse import urlparse, parse_qs, urlencode
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -40,38 +40,63 @@ def check_for_xss_list(urls, payloads):
 
     driver.quit()
 
-def check_for_xss_brute(url, payloads):
+def check_for_xss_brute(url, payloads, input_elements):
     driver = start_chromedriver()
 
     try:
         driver.get(url)
-        param_elements = driver.find_elements(By.XPATH, "//input[@type='text' or @type='search' or @type='hidden']")
-        num_params = len(param_elements)
+        parsed_url = urlparse(url)
+        query_params = parse_qs(parsed_url.query)
+        param_names = list(query_params.keys())
+        num_params = len(param_names)
         num_payloads = len(payloads)
         num_requests = 0
         num_xss_found = 0
 
-        for i, element in enumerate(param_elements, start=1):
-            param_name = element.get_attribute("name")
-            if param_name:
+        for i, param_name in enumerate(param_names, start=1):
+            param_values = query_params[param_name]
+            for param_value in param_values:
                 for payload in payloads:
                     try:
-                        url_with_payload = f"{url}?{urlencode({param_name: payload})}"
+                        url_with_payload = url.replace(f"{param_name}={param_value}", f"{param_name}={urlencode(payload)}")
                         driver.get(url_with_payload)
                         alert_present = EC.alert_is_present()(driver)
                         if alert_present:
                             print(f"XSS vulnerability found in {url_with_payload}")
-                            with open(f"{urlparse(url).netloc}_xss.txt", "a") as f:
+                            with open(f"{parsed_url.netloc}_xss.txt", "a") as f:
                                 f.write(f"{url_with_payload}\n")
                             num_xss_found += 1
-                    except UnexpectedAlertPresentException as e:
-                      pass
+                    except UnexpectedAlertPresentException:
+                        pass
                     except Exception as e:
-                      pass
-                    
+                        pass
+
                     num_requests += 1
-                    
+
                     print_statistics(i, num_params, num_payloads, num_requests, num_xss_found)
+
+        for i, element in enumerate(input_elements, start=len(param_names)+1):
+            param_name = element.get_attribute("name")
+            param_value = element.get_attribute("value")
+            if param_name and param_value:
+                for payload in payloads:
+                    try:
+                        url_with_payload = url.replace(f"{param_name}={param_value}", f"{param_name}={urlencode(payload)}")
+                        driver.get(url_with_payload)
+                        alert_present = EC.alert_is_present()(driver)
+                        if alert_present:
+                            print(f"XSS vulnerability found in {url_with_payload}")
+                            with open(f"{parsed_url.netloc}_xss.txt", "a") as f:
+                                f.write(f"{url_with_payload}\n")
+                            num_xss_found += 1
+                    except UnexpectedAlertPresentException:
+                        pass
+                    except Exception as e:
+                        pass
+
+                    num_requests += 1
+
+                    print_statistics(i, len(input_elements), num_payloads, num_requests, num_xss_found)
 
     except Exception as e:
         logging.error(f"An error occurred while processing URL: {url}", exc_info=True)
@@ -106,4 +131,8 @@ if __name__ == "__main__":
         payloads = []
         with open("payloads.txt", "r") as f:
             payloads = [line.strip() for line in f.readlines()]
-        check_for_xss_brute(url, payloads)
+        driver = start_chromedriver()
+        driver.get(url)
+        input_elements = driver.find_elements(By.XPATH, "//input[@type='text' or @type='search' or @type='hidden']")
+        driver.quit()
+        check_for_xss_brute(url, payloads, input_elements)
